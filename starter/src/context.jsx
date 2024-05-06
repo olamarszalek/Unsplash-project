c8cd76363de283106ff1a4481287152dbc6ee375
ola <ola.0608@gmail.com>
2024-05-06 22:01:25 +0200

    dark theme set-up, theme toggle set-up

--- starter/src/context.jsx
+++ starter/src/context.jsx
@@ -3,8 +3,17 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext()
 
 export const AppProvider = ({children}) => {
+
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+    const toggleDarkTheme = () => {
+       const newDarkTheme = !isDarkTheme
+       setIsDarkTheme(newDarkTheme)
+       const body = document.querySelector('body')
+       body.classList.toggle('dark-theme', newDarkTheme)
+    }
      
-    return <AppContext.Provider value={{}}>
+    return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme}}>
         {children}
     </AppContext.Provider> 
 }
