6904171727a1718703b09437b5154e86cc474a16
ola <ola.0608@gmail.com>
2024-05-07 22:10:56 +0200

    GET images function and gallery set-up

--- starter/src/Gallery.jsx
+++ starter/src/Gallery.jsx
@@ -1,8 +1,44 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import axios from 'axios'
+
+const url = "https://api.unsplash.com/search/photos?client_id=fm1h8bSsM7zYSxTcTwKW7UhnWMPy4D4z9MT0FOA_mnA&query=cats"
 
 const Gallery = () => {
+
+  const response = useQuery({
+    queryKey:['images'],
+    queryFn: async () => {
+      const result = await axios.get(url)
+      return result.data
+    }
+  })
+  if(response.isLoading){
+    return <section className='image-container'>
+      <h4>Loading...</h4>
+    </section>
+  }
+  if(response.isError){
+    return <section className='image-container'>
+    <h4>There was an error...</h4>
+  </section>
+  }
+
+  const results = response.data.results
+
+  if(results.length < 1){
+    return <section className='image-container'>
+    <h4>There was an error...</h4>
+  </section>
+  }
+  
   return (
-    <div>Gallery</div>
+    <section className='image-container'>
+      {results.map((item) => {
+        const url = item?.urls?.regular
+        return <img src={url} key={item.id} alt={item.alt_description} className='img'></img>
+      })}
+    </section>
   )
 }
 
